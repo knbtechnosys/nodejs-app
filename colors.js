0e073fdb3a5c276c4c9d879e365b2d1e512ec5c2
knbtechnosys <knbtechnosys@gmail.com>
2024-01-22 17:11:24 +0530

    nodesjs

---
+++ colors.js
@@ -0,0 +1,4 @@
+const color=require('colors');
+console.log("testin".red);
+console.log("demo color".yellow);
+console.log("testing nodemon");
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("welcome to node js");
+var a=10;
+var b=20;
+console.warn(a+b);  //warn 
